007fb6f7713340826ffb5c5ae9b2ed8ac972e7a1
x3kcl <bodo@naumann.cc>
2020-03-06 05:19:02 +0100

    install script added

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Deployment START"
+tar xjvf /root/www.tar.bz
+chown cloud.cloud -R /root/www
+mv /root/www /home/cloud/docker/
+rm -rf /home/cloud/docker/suhr
+rm /root/www.tar.bz
+mv /home/cloud/docker/www /home/cloud/docker/suhr
+chown cloud.cloud -R /home/cloud/docker/suhr
+echo "Stop docker"
+su -c "docker-compose -f /home/cloud/docker/docker-compose.yml stop" cloud
+echo "Start docker"
+su -c "docker-compose -f /home/cloud/docker/docker-compose.yml up -d" cloud
+echo "Deployment END"
