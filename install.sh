b829570bd1f3c1edaa6c3ed2e0a9e9aa6f01a2de
x3kcl <bodo@naumann.cc>
2020-03-06 05:36:59 +0100

    install script updated

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo "Deployment START"
 chown cloud.cloud -R /root/www
+echo "move www"
 mv /root/www /home/cloud/docker/
-rm -rf /home/cloud/docker/suhr
-rm /root/www.tar.bz
-mv /home/cloud/docker/www /home/cloud/docker/suhr
-chown cloud.cloud -R /home/cloud/docker/suhr
 echo "Stop docker"
 su -c "docker-compose -f /home/cloud/docker/docker-compose.yml stop" cloud
+echo "remove old folder"
+rm -rf /home/cloud/docker/suhr
+echo "rename www"
+mv /home/cloud/docker/www /home/cloud/docker/suhr
+echo "adjust rights"
+chown cloud.cloud -R /home/cloud/docker/suhr
 echo "Start docker"
 su -c "docker-compose -f /home/cloud/docker/docker-compose.yml up -d" cloud
+rm /root/www.tar.bz
 echo "Deployment END"
