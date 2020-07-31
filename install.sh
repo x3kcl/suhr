ecf512c6dfcf7eca4b0e006e6f7b54acd6b623fa
x3kcl <bodo@naumann.cc>
2020-07-31 09:39:33 +0200

    Updated deployment script

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ chown cloud.cloud -R /root/www
 echo "move www"
 mv /root/www /home/cloud/docker/
 echo "Stop docker"
-su -c "docker-compose -f /home/cloud/docker/docker-compose.yml stop" cloud &
+#su -c "docker-compose -f /home/cloud/docker/docker-compose.yml stop" cloud &
+su -c "docker-compose -f /home/cloud/docker/docker-compose.yml stop apache" cloud &
 BACK_PID=$!
 wait $BACK_PID
 #su -c "docker-compose -f /home/cloud/docker/docker-compose.yml restart" cloud
@@ -15,7 +16,8 @@ mv /home/cloud/docker/www /home/cloud/docker/suhr
 echo "adjust rights"
 chown cloud.cloud -R /home/cloud/docker/suhr
 echo "Start docker"
-su -c "docker-compose -f /home/cloud/docker/docker-compose.yml up -d" cloud &
+#su -c "docker-compose -f /home/cloud/docker/docker-compose.yml up -d" cloud &
+su -c "docker-compose -f /home/cloud/docker/docker-compose.yml up -d apache" cloud &
 BACK_PID=$!
 wait $BACK_PID
 rm /root/www.tar.bz
